2013d0fbbb4fadcd919bcf075c91b27f2d46dd67
Ankares <vasya-petrenko-99@mail.ru>
2022-11-01 16:11:35 +0300

    migrations+auth+blades

---
+++ database/migrations/2022_11_01_115025_create_vehicle_directory.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicle_directory', function (Blueprint $table) {
+            $table->id();
+            $table->string('model');
+            $table->integer('year_of_production');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicle_directory');
+    }
+};
---
+++ database/migrations/2022_11_01_115225_create_cars_showroom_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars_showroom', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('vehicle_id')->unsigned();
+            $table->foreign('vehicle_id')->references('id')->on('vehicle_directory');
+            $table->string('color');
+            $table->integer('price');
+            $table->string('sold')->default('no');
+            $table->date('sale_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars_showroom');
+    }
+};
